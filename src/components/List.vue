2e5190a170755c15acbe4a97e064676606fdb151
Jack <ajackofsometrades@gmail.com>
2018-08-27 16:52:05 -0700

    showing results of promise

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,49 @@
 <template>
   <div class="list">
     <h2>Lists Lists Lists</h2>
+    <p v-if="realTime">{{ realTime['Materials'] }}</p>
   </div>
 </template>
 
 <script>
+import AlphaVantage from '../services/AlphaVantage.js';
+
 export default {
+  data() {
+    return {
+      realTime: null,
+      loading: false,
+      error: null,
+      search: '',
+      total: 0
+    };
+  },
+  created() {
+    this.sectors = AlphaVantage.getSectors();
+    this.handleSearch();
+
+  },
+  methods: {
+    handleSearch() {
+      // this.search = search;
+      this.searchSectors();
+    },
+
+    searchSectors() {
+      this.loading = true;
+      this.error = null;
+
+      AlphaVantage.getSectors()
+        .then(response => {
+          this.realTime = response['Rank A: Real-Time Performance'];
+          this.loading = false;
+        })
+        .catch(err => {
+          this.error = err.message;
+          this.loading = false;
+        });
+    }
+  }
 
 };
 </script>
---
+++ src/services/AlphaVantage.js
@@ -0,0 +1,32 @@
+
+
+export default {
+  // return the promise, which is the final promise
+  // after calling response.json(). This means:
+  // 1) Web Request is made to server
+  // 2) _when_ browser gets response, _then_ call the callback
+  // 3) The return value of that _then_ callback is passed to the next then (outside this function)
+  getSectors(searchTerm = '') {
+
+    // url:
+    // 1) protocol (https://)
+    // 2) host server  (swapi.co)
+    // 3) path (/api/people)
+    // 4) search or query
+    //    - separated from path via "?"
+    //    - name=value pairs separated by "&"
+    //    - use "encodeURIComponent" to escape search values:
+    //    ?APIKEY=abc123&search=${encodeURIComponent(name)}
+
+    // if(searchTerm === 'jar') return Promise.reject(new Error('meesa think that bad search'));
+    searchTerm;
+    return fetch(`https://www.alphavantage.co/query?function=SECTOR&apikey=XPMJIH1MNBTH8FE0`)
+    // return fetch(`https://swapi.co/api/people?search=${encodeURIComponent(searchTerm)}`)
+      /* deserialize the data to real JavaScript object! */
+
+      .then(response => {
+        return response.json();
+      });
+  }
+};
+
