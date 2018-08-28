4f851ac298887acac914b339e467b5e54af077c4
Jack <ajackofsometrades@gmail.com>
2018-08-27 23:08:17 -0700

    Updating header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
   <header>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>SEARCH THE WEB</h1>
+    <img alt="Vue logo" src="../assets/logo2.png">
+    
+    <div>
+      <h1>Simply $tock$</h1>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/industries">Industry Performance</router-link>
+        <router-link to="/stocks">Stock Performance</router-link>
+      </nav>
+    </div>
 
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/list">List</router-link>
-    </nav>
+
   </header>
 </template>
 
@@ -16,6 +21,56 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css?family=Monoton');
+
+img {
+  width: 200px;
+  height: 200px;
+}
+
+a {
+  font-size: 24px;
+  padding: 10px;
+  margin: 15px;
+  border: 1px solid black;
+  text-decoration: none;
+  color: white;
+  border-radius: 5px;
+  box-shadow: 0.5px 0.5px 0.5px black;
+  background-color: #00b061
+
+}
+
+a:hover {
+  background-color: #0c8d53;
+  box-shadow: 1.5px 1.5px 1.5px black;
+  transform: scale(1.5);
+}
+
+h1 {
+  font-family: 'Monoton', cursive;
+  font-size: 74px;
+  font-weight: normal;
+  margin: 5px;
+  text-transform: uppercase;
+}
+
+nav {
+  padding: 20px;
+}
+
+header {
+  display: grid;
+  grid-template-columns: 25% 50% 25%;
+  box-shadow: 3px 2px 2px black;
+
+  background: #000000; /* Old browsers */
+  background: -moz-linear-gradient(left, #000000 0%, #00b061 40%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(left, #000000 0%,#00b061 40%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to right, #000000 0%,#00b061 40%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='#00b061',GradientType=1 );
+}
 
 </style>
