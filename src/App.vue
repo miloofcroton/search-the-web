385109ff015836a303ab03094831d772c3672f21
Jack <ajackofsometrades@gmail.com>
2018-08-27 23:08:03 -0700

    adding footer

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,31 @@
   <div id="app">
     <Header/>
     <router-view></router-view>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header, Footer
   }
 };
 </script>
 
-<style>
+<style scoped>
+
+#app {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  height: 100vh;
+  width: 100vw;
+}
+
 
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <img alt="Vue logo" src="../assets/logo2.png">
+    <p>© Simply Stocks - 2018</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+img {
+  width: 50px;
+  padding-top: 10px;
+}
+footer {
+  background-color: black;
+  color: rgb(189, 189, 189);
+}
+
+</style>
