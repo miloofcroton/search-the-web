ef13bd56642ec7a64f6a10e87d5c00ab7186fcac
Jack <ajackofsometrades@gmail.com>
2018-08-27 23:08:59 -0700

    adding stock search

---
+++ src/components/StockSearch.vue
@@ -0,0 +1,104 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div class="form">
+      <p>Symbol:</p>
+      <input v-model="symbol" placeholder="GOOG" required>
+      
+      <p>Time Period:</p>
+      <select v-model="period" required>
+        <option disabled value="">Please select one</option>
+        <option value="TIME_SERIES_DAILY">Daily</option>
+        <option value="TIME_SERIES_WEEKLY">Weekly</option>
+        <option value="TIME_SERIES_MONTHLY">Monthly</option>
+      </select>
+
+      <p>Date (ex: "2018-08-27"):</p>
+      <input v-model="date" type="date" placeholder="2018-08-27" required>
+      
+      <p>Time of Day:</p>
+      <select v-model="time" required>
+        <option disabled value="">Please select one</option>
+        <option value="1. open">Open</option>
+        <option value="2. high">High</option>
+        <option value="3. low">Low</option>
+        <option value="4. close">Close</option>
+      </select>
+    </div>
+
+    <button type="submit">Add</button>
+    <button type="reset">Reset</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    onSearch: Function
+  },
+  data() {
+    return {
+      symbol: '',
+      period: '',
+      date: '',
+      time: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.onSearch(this.symbol, this.date, this.time, this.period);
+      this.symbol = '';
+      this.period = '';
+      this.date = '';
+      this.time = '';
+    }
+  }
+};
+</script>
+
+<style>
+
+div.form {
+  width: 50%;
+  margin: 10px auto;
+  display: grid;
+  grid-template-columns: auto auto;
+}
+
+div.form p {
+  justify-self: end;
+  margin: 10px;
+}
+div.form input,select {
+  justify-self: start;
+}
+
+
+form {
+  padding: 20px;  
+}
+
+select {
+  padding: 8px;
+  border-radius: 8px;
+  margin-right: 4px;
+  cursor: pointer;
+}
+
+input {
+  padding: 6px;
+  border-radius: 6px;
+}
+
+button {
+  padding: 6px;
+  border-radius: 8px;
+  margin-right: 4px;
+  cursor: pointer;
+  width: 60px;
+}
+
+button:hover {
+  background: grey;
+}
+
+</style>
---
+++ src/components/Stocks.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="list">
+    <h2>Compare Stocks</h2>
+    <p>Add all of the stocks that you want to compare</p>
+
+    <StockSearch :onSearch="handleSearch"/>
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Symbol</th>
+          <th>Date</th>
+          <th>Time of Day</th>
+          <th>Price Period</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody v-if="symbolArr">
+        <tr v-for="(item, index) in symbolArr" 
+          :key="valueArr[index]"
+        >
+          <td>{{ symbolArr[index] }}</td>
+          <td>{{ dateArr[index] }}</td>
+          <td>{{ timeNameArr[index] }}</td>
+          <td>{{ periodNameArr[index] }}</td>
+          <td>${{ valueArr[index] }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-if="value">
+      <p>The {{periodName}} value for {{symbol}} on {{date}} at {{timeName}} was {{ value }}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import AlphaVantage from '../services/AlphaVantage.js';
+import StockSearch from './StockSearch.vue';
+
+export default {
+  components: {
+    StockSearch,
+  },
+  data() {
+    return {
+      realTime: null,
+      loading: false,
+      error: null,
+
+      symbol: '',
+      date: '',
+      time: '',
+      period: '',
+      periodName: null,
+      timeName: null,
+      periodSearch: null,
+      value: null,
+
+      symbolArr: [],
+      dateArr: [],
+      timeNameArr: [],
+      periodNameArr: [],
+      valueArr: []
+    };
+  },
+  created() {
+  },
+  methods: {
+    handleSearch(symbol, date, time, period) {
+      this.symbol = symbol;
+      this.date = date;
+      this.time = time;
+      this.period = period;
+      this.periodName = period.replace(/^[^*]+_\s*/g, '').toLowerCase();
+      this.timeName = time.replace(/^[^*]+\s{1}/g, '');
+      if(this.period === 'TIME_SERIES_DAILY'){
+        this.periodSearch = 'Time Series (Daily)';
+      }
+      if(this.period === 'TIME_SERIES_WEEKLY'){
+        this.periodSearch = 'Weekly Time Series';
+      }
+      if(this.period === 'TIME_SERIES_MONTHLY'){
+        this.periodSearch = 'Monthly Time Series';
+      }
+
+      this.searchStock();
+    },
+
+    searchStock() {
+      this.loading = true;
+      this.error = null;
+
+      AlphaVantage.getStock(this.symbol, this.period)
+        .then(response => {
+          this.value = response[this.periodSearch][this.date][this.time];
+          console.log('response:', response);
+          console.log('this.value:', this.value);
+
+          this.valueArr.push(this.value);
+          this.symbolArr.push(this.symbol);
+          this.dateArr.push(this.date);
+          this.timeNameArr.push(this.timeName);
+          this.periodNameArr.push(this.periodName);
+          
+          this.loading = false;
+        })
+        .catch(err => {
+          this.error = err.message;
+          this.loading = false;
+        });
+    }
+
+  }
+
+};
+</script>
+
+<style scoped>
+
+table {
+  margin: 5px auto;
+}
+
+h2 {
+  font-size: 34px;
+}
+
+p {
+  font-style: oblique;
+}
+
+th, td {
+  padding: 5px;
+  margin: 2px;
+  font-size: 24px;
+  border: 1px solid black;
+}
+
+</style>
+
